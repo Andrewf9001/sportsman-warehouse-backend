cadb1c51517b14de8b545aae4872d23cc5aa2548
Andrewf9001 <andrewf9001@gmail.com>
2025-06-06 14:31:53 -0600

    added env, added jwt secret key, added jwtmanager to config, initialized in create_app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 
-from config import db, ma
+from config import db, ma, jwt
 from routes.store_routes import store_routes
 
 
@@ -10,9 +11,12 @@ def create_app():
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sportsman-warehouse.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 
     db.init_app(app)
     ma.init_app(app)
+    jwt.init_app(app)
+
     CORS(app, origins=["http://localhost:5173"])
     
     app.register_blueprint(store_routes, url_prefix="/api/stores")
