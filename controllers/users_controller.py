f04567b78e12cc1abe6ecdbe54f3b82f3bd33dc1
Andrewf9001 <andrewf9001@gmail.com>
2025-06-09 15:04:42 -0600

    added addition user fields, added them to register route

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -5,13 +5,16 @@ from models.users import Users, user_schema
 
 def register():
     data = request.get_json()
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    user_name = data.get("user_name")
     email = data.get("email")
     password = data.get("password")
 
     if Users.query.filter_by(email=email).first():
         return jsonify({"msg": "Email already exists"}), 400
     
-    new_user = Users(email=email)
+    new_user = Users(first_name, last_name, user_name, email)
     new_user.set_password(password)
 
     db.session.add(new_user)
--- models/users.py
+++ models/users.py
@@ -4,9 +4,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from config import db, ma
 
 class Users(db.Model):
-    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    user_name = db.Column(db.String)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+
+    def __init__(self, first_name, last_name, user_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.email = email
+        self.password = password
+        
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
