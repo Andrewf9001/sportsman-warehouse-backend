f9d93f62bb5271c0e8f32004847c791b2ff8c0b9
Andrewf9001 <andrewf9001@gmail.com>
2025-06-09 16:04:15 -0600

    setup comment for product structure

---
+++ models/products.py
@@ -0,0 +1,6 @@
+"""
+    - Manage Main Category (Hunting, Fishing, etc..)
+    - Manage each section within the category (Archery, Optics, etc..)
+    - Create Model for MenuLinks (Bows, Arrows, etc..)
+    - Setup Products Model
+"""
