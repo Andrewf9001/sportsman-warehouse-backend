35dddbf559f7d2b796838155156f945b26477418
Andrewf9001 <andrewf9001@gmail.com>
2025-06-13 13:12:57 -0600

    added products model, building diagram for product layout

--- models/products.py
+++ models/products.py
@@ -1,6 +1,23 @@
-"""
-    - Manage Main Category (Hunting, Fishing, etc..)
-    - Manage each section within the category (Archery, Optics, etc..)
-    - Create Model for MenuLinks (Bows, Arrows, etc..)
-    - Setup Products Model
-"""
+import uuid
+from config import db, ma
+
+class Products(db.Model):
+    product_id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    category = db.Column(db.String, nullable=False)
+    name = db.Column(db.String)
+    brand = db.Column(db.String)
+    description = db.Column(db.String)
+    price = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
+
+    def __init__(self, category):
+        self.category = category
+
+class ProductsSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Products
+        load_instance = True
+
+
+store_schema = ProductsSchema()
+stores_schema = ProductsSchema(many=True)
